ac2d030c192905238a78c9424857c7493fc063d0
Kevin Dolan <kevin@kevin-desktop>
2010-03-10 22:02:05 -0500

    im so sad

--- src/com/google/gwt/ddmvc/model/update/CascadeUpdate.java
+++ src/com/google/gwt/ddmvc/model/update/CascadeUpdate.java
@@ -1,7 +1,7 @@
 package com.google.gwt.ddmvc.model.update;
 
 /**
- * Update sent as a notification when a data change is cascaded
+ * Update sent as a notification when a data change is cascaded 
  * to a computed model.  At this time, there is no way to know
  * the source of a cascaded data change.
  * If this model update were actually passed to a model, it
